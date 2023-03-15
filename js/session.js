67adae0883e01ba4714c3c8af568c79910f4b099
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2023-03-15 17:45:37 +0000

    Añadidas validaciones de formulario del session.html,falta el input repeatPassword.

--- js/session.js
+++ js/session.js
@@ -2,16 +2,66 @@ w3IncludeHTML(loadPage);
 
 //elements
 
-let passwordConfirm;
 let fullName;
+let mail;
+let password;
+let passwordConfirm;
+let sessionOptions;
 
-function loadPage(path){
+function loadPage(){
     let changeButton = document.getElementById("changeButton");
-    passwordConfirm = document.querySelector("#sessionForm ul").childNodes[7];
-    fullName = document.querySelector("#sessionForm ul").childNodes[1];
-    
+    let list = document.querySelector("#sessionForm ul").childNodes;
+    fullName = list[1];
+    mail = list[3];
+    password = list[5];
+    passwordConfirm = list[7];
+    sessionOptions = list[9];
     changeButton.addEventListener("click",() => changePage(changeButton));
+    let inputs = document.querySelectorAll("input");
     
+    for(let i = 0;i<inputs.length;i++) {
+        formValidate(inputs[i])
+        inputs[i].addEventListener("change", 
+            function(event) {
+                event.target.classList.add('interacted'); 
+
+                event.target.setCustomValidity("");
+                
+                formValidate(event.target);
+
+            }
+        );
+
+    }   
+}
+
+
+function formValidate(inputElement){
+    
+    let validity = inputElement.validity;
+  
+    if(!validity.valid){
+    
+        validity.valueMissing === true ? resolveValidation(inputElement,"valueMissing"):
+
+        resolveValidation(inputElement,"patternMismatch");
+
+    }
+
+}
+
+function resolveValidation(inputElement, error){
+    switch(error){
+        case "valueMissing":
+            inputElement.setCustomValidity("This field is required");
+            break;
+        
+        case "patternMismatch":
+            console.log(inputElement)
+            inputElement.name == "passwordRepeat" ? checkPasswordConfirmation(document.querySelector("#password"),inputElement):
+            inputElement.setCustomValidity(inputElement.title);
+            break;
+    }
 
 }
 
@@ -19,6 +69,7 @@ function changePage(button){
     let textChange = document.querySelector("#changeSessionOptions").querySelector("p");
     let submitButton = document.querySelector("#submit");
     let formList = document.querySelector("#sessionForm ul");
+    console.log(sessionOptions);
     let title = document.querySelector("#titleForm");
     switch(button.value){
         case "log in":
@@ -31,14 +82,16 @@ function changePage(button){
             `If you don't have an account,<br/>please  ${button.outerHTML}`;
             console.log(button.outerHTML);
             break;
+
         default:
             button.value = "log in";
             submitButton.value = "Sign up";
-            title.innerHTML = "CREATE ACCOUNT"
-            textChange.innerHTML = 
+            title.innerHTML = "CREATE ACCOUNT";
+            formList.insertBefore(fullName,mail);
+            formList.insertBefore(passwordConfirm,sessionOptions);
+            textChange.innerHTML =
             `If you have already an account,<br /> please ${button.outerHTML}` ;
             console.log(button.outerHTML);
-            
     }
     document.querySelector("#changeButton").
     addEventListener("click",() => changePage(changeButton));
