993e8676dad589de94880eab2cb1d78c78f5a8b8
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2023-03-18 13:33:49 +0000

    Código de session.js refactorizado.

--- js/session.js
+++ js/session.js
@@ -1,46 +1,18 @@
 w3IncludeHTML(loadPage);
 
-//elements
+// ul elements
+let fullNameLi;
+let mailLi;
+let passwordLi;
+let passwordConfirmLi;
+let sessionOptionsLi;
 
-let fullName;
-let mail;
-let password;
-let passwordConfirm;
-let sessionOptions;
+
 
 function loadPage(){
-    let changeButton = document.getElementById("changeButton");
-    let list = document.querySelector("#sessionForm ul").childNodes;
-    fullName = list[1];
-    mail = list[3];
-    password = list[5];
-    passwordConfirm = list[7];
-    sessionOptions = list[9];
-    changeButton.addEventListener("click",() => changePage(changeButton));
-    document.querySelector("#submit").addEventListener("click",function(event){
-        //event.preventDefault();
-        let passwordRepeat = document.querySelector("#passwordRepeat");
-        if( passwordRepeat.value !== document.querySelector("#password").value) passwordRepeat.setCustomValidity("Passwords don't match.");
-        else passwordRepeat.setCustomValidity("");
-    })
-    let inputs = document.querySelectorAll("input");
-    
-    for(let i = 0;i<inputs.length;i++) {
-        formValidate(inputs[i])
-        inputs[i].addEventListener("blur", 
-            function(event) {
-                event.target.classList.add('interacted'); 
-
-                event.target.setCustomValidity("");
-                
-                formValidate(event.target);
-
-            }
-        );
-
-    }   
+    getUlElements(document.querySelector("#sessionForm ul").childNodes);
+    addEventsListener();
 }
-
 
 function formValidate(inputElement){
     
@@ -69,36 +41,68 @@ function resolveValidation(inputElement, error){
 
 }
 
-function checkPasswordConfirmation(original, confirmation ){
-    if(original.value !== confirmation.value) alert("Password do not match");
-     
+function getUlElements(ul){
+    fullNameLi = ul[1];
+    mailLi = ul[3];
+    passwordLi = ul[5];
+    passwordConfirmLi = ul[7];
+    sessionOptionsLi = ul[9];
+}
+
+function addEventsListener(){
+
+    document.querySelector("#changeButton").addEventListener("click",() => changePage(changeButton));
+
+    document.querySelector("#submit").addEventListener("click",checkPasswordRepeat);
+
+    let inputs = document.querySelectorAll("input");
+    for(let i = 0;i<inputs.length;i++) {
+        formValidate(inputs[i]);
+        inputs[i].addEventListener("blur", 
+            function(event) {
+                event.target.classList.add('interacted'); 
+
+                event.target.setCustomValidity("");
+                
+                formValidate(event.target);
+
+            }
+        );
+    }  
+}
+
+function checkPasswordRepeat(){
+    let passwordRepeat = document.querySelector("#passwordRepeat");
+    if( passwordRepeat.value !== document.querySelector("#password").value) passwordRepeat.setCustomValidity("Passwords don't match.");
+    else passwordRepeat.setCustomValidity("");
+}
+
+function changeContent(buttonValue,submitButtonValue,titleInner,textChangeInner){
+    let textChange = document.querySelector("#changeSessionOptions").querySelector("p");
+    let submitButton = document.querySelector("#submit");
+    let title = document.querySelector("#titleForm");
+    let changeButton = document.querySelector("#changeButton");
+
+    changeButton.value = buttonValue;
+    submitButton.value = submitButtonValue;
+    title.innerHTML = titleInner;
+    textChange.innerHTML = textChangeInner + `${changeButton.outerHTML}`;
+
 }
 
 function changePage(button){
-    let textChange = document.querySelector("#changeSessionOptions").querySelector("p");
-    let submitButton = document.querySelector("#submit");
     let formList = document.querySelector("#sessionForm ul");
-    let title = document.querySelector("#titleForm");
     switch(button.value){
         case "log in":
-            button.value = "sign up";
-            submitButton.value = "Log in";
-            title.innerHTML = "LOG IN"
-            passwordConfirm = formList.removeChild(passwordConfirm);
-            fullName = formList.removeChild(fullName);
-            textChange.innerHTML = 
-            `If you don't have an account,<br/>please  ${button.outerHTML}`;
+            changeContent("sign up","Log in","LOG IN",`If you don't have an account,<br/>please `);
+            passwordConfirmLi = formList.removeChild(passwordConfirmLi);
+            fullNameLi = formList.removeChild(fullNameLi);
             break;
 
         default:
-            button.value = "log in";
-            submitButton.value = "Sign up";
-            title.innerHTML = "CREATE ACCOUNT";
-            formList.insertBefore(fullName,mail);
-            formList.insertBefore(passwordConfirm,sessionOptions);
-            textChange.innerHTML =
-            `If you have already an account,<br /> please ${button.outerHTML}` ;
+            changeContent("log in","Sign up","CREATE ACCOUNT",`If you have already an account,<br /> please `)
+            formList.insertBefore(fullNameLi,mailLi);
+            formList.insertBefore(passwordConfirmLi,sessionOptionsLi);
     }
-    document.querySelector("#changeButton").
-    addEventListener("click",() => changePage(changeButton));
+    document.querySelector("#changeButton").addEventListener("click",() => changePage(changeButton));
 }
