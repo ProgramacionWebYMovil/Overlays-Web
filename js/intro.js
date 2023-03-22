377ab6384d05b73fddb8063ff425c23c9926fb36
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2023-03-22 16:39:02 +0000

    Añadida información al footer desde footer-info.json

--- js/intro.js
+++ js/intro.js
@@ -3,19 +3,19 @@
 //Creo una solicitud
 //let xhr = new XMLHttpRequest();
 /*GET para pedir informacion
- * la imformación que pido
+ * la imformaciï¿½n que pido
  * true para decir que es asincrono*/
 //xhr.open('GET', "/templates/header.html", true);
 //Send para mandar la solicitud
 //xhr.send();
 
-/*Dos opciones distintas de añadir el listener a un metodo*/
+/*Dos opciones distintas de aï¿½adir el listener a un metodo*/
 //xhr.addEventListener("readystatechange", processRequest, false);
 //xhr.onreadystatechange = processRequest;
 
-/*Metodo que hará el procesamiento*/
+/*Metodo que harï¿½ el procesamiento*/
 //function processRequest(e) {
-    /*La respuesta tiene unos códgos, cuando esta en 4 es que todo se ha completado
+    /*La respuesta tiene unos cï¿½dgos, cuando esta en 4 es que todo se ha completado
      * el codigo de status es 200 porque es retornado del servidor cuando la solicitud ha sido exitosa*/
 //    if (xhr.readyState == 4 && xhr.status == 200) {
         //xhr.responseText para obtener el texto de la solicitud
@@ -38,10 +38,10 @@ function loadTemplate(fileName, id) {
 
     //FETCH 
     fetch(fileName)
-        //Primer .then se especifica que queremos del objeto (html) bruto que nos devolverá la llamada fetch
+        //Primer .then se especifica que queremos del objeto (html) bruto que nos devolverï¿½ la llamada fetch
         .then((response) => { return response.text(); })
         /*Segundo .then se llama despues de que el primero se haya completado
-         * se procesa la información retornada*/
+         * se procesa la informaciï¿½n retornada*/
         .then((text) => {
             //console.log(text);
             text = text.split("</head>");
@@ -57,7 +57,6 @@ function fetchJson(json) {
             return data.json()
         })
         .then(items => {
-            console.log(items);
             loadJson(items);
         })
 }
@@ -68,7 +67,7 @@ function loadJson(json) {
     var language = window.navigator.language.toString();
     if (language.includes("en")) {
         data = json.english;
-    //Español
+    //Espaï¿½ol
     } else {
         data = json.spanish;
     }
@@ -76,7 +75,7 @@ function loadJson(json) {
     let body = document.querySelector('body');
     /*Recorro el json
      * data es json.english
-     * key será cada clave
+     * key serï¿½ cada clave
      * data[key] es el valor de cada clave
      */
     for (key in data) {
