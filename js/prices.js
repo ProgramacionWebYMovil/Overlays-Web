e0fa9a1f23aff369298abb69a18732f93e3efc4d
jpere <jpereiro1@gmail.com>
2023-03-22 20:53:20 +0000

    Añadir la carga del json en el prices

--- js/prices.js
+++ js/prices.js
@@ -27,6 +27,81 @@ function irAPagina() {
     window.location.href = "payment.html";
 }
 
+//METODO PARA FETCH
+function loadTemplate(fileName, id, js) {
+
+    //FETCH 
+    fetch(fileName)
+        //Primer .then se especifica que queremos del objeto (html) bruto que nos devolver� la llamada fetch
+        .then((response) => { return response.text(); })
+        /*Segundo .then se llama despues de que el primero se haya completado
+         * se procesa la informaci�n retornada*/
+        .then(async (text) => {
+            //console.log(text);
+            text = text.split("</head>");
+
+            document.querySelector("head").innerHTML += text[0].replace("<head>", "");
+            document.querySelector(id).innerHTML = text[1];
+
+            if (js) loadJs(js);
+        });
+
+
+}
+
+function loadJs(js) {
+    let footerScript = document.createElement("script");
+
+    fetch(js).then(response => {
+        return response.text();
+    }).then(data => {
+        footerScript.innerHTML = data;
+
+        document.head.appendChild(footerScript);
+
+        loadFooterContent();
+
+    })
+}
+
+
+function loadJson(json) {
+    var data;
+    //Ingles
+    var language = window.navigator.language.toString();
+    if (language.includes("en")) {
+        data = json.english;
+        //Espa�ol
+    } else {
+        data = json.spanish;
+    }
+
+    let body = document.querySelector('body');
+    /*Recorro el json
+     * data es json.english
+     * key ser� cada clave
+     * data[key] es el valor de cada clave
+     */
+    for (key in data) {
+        body.querySelector("#" + key).innerHTML = data[key];
+    }
+}
+
+
+function fetchJson(json) {
+    fetch(json)
+        .then(data => {
+            return data.json()
+        })
+        .then(items => {
+            loadJson(items);
+        })
+}
+
+loadTemplate("/templates/header.html", "header");
+loadTemplate("/templates/footer.html", "footer", "/js/footer.js");
+fetchJson("/json/prices.json");
+
 
 
 
